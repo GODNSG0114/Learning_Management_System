a8e90c48a7f726a65ccaf99a1e6a18c4e6efddab
Nikhil God <nikhil0114@gmail.com>
2025-09-20 17:32:04 +0530

    Refactor CourseCard and CourseSection components for improved layout and functionality; add calculateRating method to AppContext; enhance Navbar navigation; create initial DashBoard component.

--- client/src/Components/Student/CourseSection.jsx
+++ client/src/Components/Student/CourseSection.jsx
@@ -11,14 +11,14 @@ const CourseSection = () => {
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3 '>Discover our top-rated
         courses across various categories.
-        From coding and design to business and wellness, our courses are crafted to deliver results.
+        From coding and design to <br /> business and wellness, our courses are crafted to deliver results.
       </p>
 
-      <div>
-        {allCourses.slice(0, 4).map((course, index) => {
-         return <CourseCard key={index} course={course} />
-          
-        })}
+      <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
+                gap-4 px-4 md:px-0 md:my-16 my-10">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
       </div>
 
       <Link to={'/course-list'} onClick={() => scrollTo(0, 0)}
