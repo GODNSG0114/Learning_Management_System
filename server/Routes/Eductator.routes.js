41f5ee4bac702245b08306f4d93ec5a59498fb44
Nikhil God <nikhil0114@gmail.com>
2025-10-03 05:17:19 +0530

    Add getEducatorCourses endpoint to retrieve courses for the authenticated educator

--- server/Routes/Eductator.routes.js
+++ server/Routes/Eductator.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addCourse, updateRoleToEducator } from '../Controlles/Educator.Controller.js'
+import { addCourse, getEducatorCourses, updateRoleToEducator } from '../Controlles/Educator.Controller.js'
 import upload from '../Config/multer.js'
 import { protectEducator } from '../Middleware/authMiddleware.js'
 
 const educatorRouter = express.Router()
 educatorRouter.get('/update-role' , updateRoleToEducator)
 educatorRouter.post('/add-course',upload.single('image'),protectEducator ,addCourse)
+educatorRouter.get('/courses',protectEducator ,getEducatorCourses)
 export default educatorRouter;
