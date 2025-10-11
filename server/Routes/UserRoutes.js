dedb3a6cca76e503648f7acaf37c418700605a1f
Nikhil God <nikhil0114@gmail.com>
2025-10-11 10:30:36 +0530

    chore: add @clerk/clerk-sdk-node dependency to package.json and package-lock.json

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { addUserRating, getUserData, getUserProgress, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../Controlles/UserController.js'
-
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
 const userRouter = express.Router()
 
 userRouter.get('/data' , getUserData)
 userRouter.get('/enrolled-courses' , userEnrolledCourses)
-userRouter.post('/purchase' , purchaseCourse)
+userRouter.post('/purchase' ,ClerkExpressRequireAuth(), purchaseCourse)
 
 userRouter.post('/update-course-progress' , updateUserCourseProgress)
 userRouter.post('/get-course-progress' , getUserProgress)
